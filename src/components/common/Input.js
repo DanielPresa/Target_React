76c8c958cb75def0167bc23c595f5097d8ee70d9
Daniel Presa <pablo.d.presa@gmail.com>
2021-03-25 15:01:31 -0300

    Corrections from CR done once more.

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -18,10 +18,9 @@ const Input = ({ label, name, value, onChange, errors, active, touched, ...props
         <input name={name} value={value} onChange={onChange} {...props} />
         {touched && errors && (
           <span>
-            <br></br>
             <FormattedMessage
               id={parseInputErrors(errors)}
-              defaultMessage= {parseInputErrors(errors)}
+              defaultMessage={parseInputErrors(errors)}
             />
           </span>
         )}
