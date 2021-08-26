93e6a225b542d769e075a1f6ba4a5c2053de7659
DanielPresa <pablo.d.presa@gmail.com>
2021-08-26 15:13:16 -0300

    Added function to have map request for location in browser and to update

--- src/components/user/HomeMap.js
+++ src/components/user/HomeMap.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { MapContainer, TileLayer, Marker, Popup } from '@monsonjeremy/react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const HomeMap = () => {
-  const position = [-34.907, -56.203];
+const DEFAULT_LONGITUDE = -34.907;
+const DEFAULT_LATITUDE = -56.203;
+
+function HomeMap() {
+  
+  let latitude = DEFAULT_LATITUDE;
+  let longitude = DEFAULT_LONGITUDE;
+  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position =>
+      setPosition({ x: position.coords.latitude, y: position.coords.longitude })
+    );
+  }, [latitude, longitude]);
 
   return (
     <div id="mapContainer">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '100vh' }}>
+      <MapContainer
+        center={[latitude, longitude]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: '100vh' }}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={[latitude, longitude]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
@@ -21,5 +37,5 @@ const HomeMap = () => {
       </MapContainer>
     </div>
   );
-};
+}
 export default HomeMap;
